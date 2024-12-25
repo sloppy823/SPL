7f068da0fdac153ee72cbd378d31cb3cd6dfb195
Yair Avisar <73329084+sloppy823@users.noreply.github.com>
2024-12-25 22:48:05 +0200

    part of message bus havent finshed

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -1,4 +1,10 @@
 package bgu.spl.mics;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.*;
+
 
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
@@ -6,16 +12,34 @@ package bgu.spl.mics;
  * Only private fields and methods can be added to this class.
  */
 public class MessageBusImpl implements MessageBus {
+	private final ConcurrentHashMap<Class<? extends Message>, Queue<MicroService>> eventSubscribers;
+	private final Map<Class<? extends Broadcast>, List<MicroService>> broadcastSubscribers;
+	private final Map<MicroService, BlockingQueue<Message>> microServiceQueues;
+
+
+	private MessageBusImpl() {
+		eventSubscribers = new ConcurrentHashMap<>();
+		broadcastSubscribers = new ConcurrentHashMap<>();
+		microServiceQueues = new ConcurrentHashMap<>();
+	}
 
 	@Override
-	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-		// TODO Auto-generated method stub
-
+	public synchronized  <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
+		eventSubscribers.putIfAbsent(type, new LinkedList<>());
+		Queue<MicroService> subscribersQueue = eventSubscribers.get(type);
+		if (!subscribersQueue.contains(m)) {
+			subscribersQueue.offer(m);
+		}
 	}
 
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		// TODO Auto-generated method stub
+		broadcastSubscribers.putIfAbsent(type, new LinkedList<>());
+		List<MicroService> subscribersList = broadcastSubscribers.get(type);
+		if (!subscribersList.contains(m)) {
+			subscribersList.add(m);
+		}
+
 
 	}
 
@@ -31,17 +55,20 @@ public class MessageBusImpl implements MessageBus {
 
 	}
 
-	
+
 	@Override
 	public <T> Future<T> sendEvent(Event<T> e) {
-		// TODO Auto-generated method stub
+		//take the microservice that is subscribed to the event type from the queue
+		//eventSubscriber.get(e).handleEvent(e);
 		return null;
 	}
 
 	@Override
 	public void register(MicroService m) {
-		// TODO Auto-generated method stub
-
+		if (microServiceQueues.containsKey(m)) {
+			throw new IllegalStateException("MicroService is already registered.");
+		}
+		microServiceQueues.put(m, new LinkedBlockingQueue<>());
 	}
 
 	@Override
@@ -56,6 +83,6 @@ public class MessageBusImpl implements MessageBus {
 		return null;
 	}
 
-	
+
 
 }
