34fb5d56ff4ae6cc4d581136144deade4176b9a9
DarkZoneGod8 <yuvali.israel@gmail.com>
2025-01-02 15:14:07 +0200

    poseService

--- src/main/java/bgu/spl/mics/application/services/PoseService.java
+++ src/main/java/bgu/spl/mics/application/services/PoseService.java
@@ -1,8 +1,11 @@
 package bgu.spl.mics.application.services;
 
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.messages.PoseEvent;
+import bgu.spl.mics.application.messages.TerminatedBroadcast;
+import bgu.spl.mics.application.messages.TickBroadcast;
 import bgu.spl.mics.application.objects.GPSIMU;
-
+import bgu.spl.mics.application.objects.Pose;
 
 /**
  * PoseService is responsible for maintaining the robot's current pose (position and orientation)
@@ -10,14 +13,16 @@ import bgu.spl.mics.application.objects.GPSIMU;
  */
 public class PoseService extends MicroService {
 
+    private final GPSIMU gpsimu;
+
     /**
      * Constructor for PoseService.
      *
      * @param gpsimu The GPSIMU object that provides the robot's pose data.
      */
     public PoseService(GPSIMU gpsimu) {
-        super("Change_This_Name");
-        // TODO Implement this
+        super("PoseService");
+        this.gpsimu = gpsimu;
     }
 
     /**
@@ -26,6 +31,21 @@ public class PoseService extends MicroService {
      */
     @Override
     protected void initialize() {
-        // TODO Implement this
+        // Subscribe to TickBroadcast
+        subscribeBroadcast(TickBroadcast.class, tickBroadcast -> {
+            // Update the current tick from the broadcast
+            int currentTick = tickBroadcast.getTick();
+            gpsimu.setCurrentTick(currentTick);
+
+            // Get the current pose and send a PoseEvent
+            Pose currentPose = gpsimu.getPoseList().get(gpsimu.getPoseList().size() - 1);
+            sendEvent(new PoseEvent(currentPose, currentTick));
+        });
+
+        // Subscribe to TerminatedBroadcast for shutdown signals
+        subscribeBroadcast(TerminatedBroadcast.class, termination -> {
+            System.out.println(getName() + " received TerminationBroadcast. Terminating.");
+            terminate();
+        });
     }
 }
